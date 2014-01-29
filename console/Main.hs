c842ea0a0882a97ccfe1e98109bdde66a233773a
Edward Kmett <ekmett@gmail.com>
2014-01-29 04:19:44 -0500

    Updated console app to start ekg

--- console/Main.hs
+++ console/Main.hs
@@ -1,4 +1,17 @@
 module Main where
 
+import Data.Monoid
+import Options.Applicative
+import Rogue.Monitor
+
 main :: IO ()
-main = putStrLn "TODO"
+main = do
+  options <- execParser $ info parseMonitorOptions $
+    fullDesc
+    <> progDesc "rogue.console"
+    <> header "A game console"
+
+  withMonitor options $ \_mon -> do
+    putStrLn "Press enter to continue."
+    _ <- getLine
+    return ()
