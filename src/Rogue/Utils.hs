7bd13a7261da1614b3bb73cd87279c5c455c66ea
Edward Kmett <ekmett@gmail.com>
2014-02-01 15:14:11 -0500

    Style match

--- src/Rogue/Utils.hs
+++ src/Rogue/Utils.hs
@@ -1,28 +1,25 @@
-module Rogue.Utils (
-    isqrt
+module Rogue.Utils
+  ( isqrt
   , concatMapM
   ) where
 
 import Data.Int
 import Control.Monad
 
-
 -- https://ghc.haskell.org/trac/ghc/ticket/2042
-concatMapM        :: (Monad m) => (a -> m [b]) -> [a] -> m [b]
-concatMapM f xs   =  liftM concat (mapM f xs)
+concatMapM :: Monad m => (a -> m [b]) -> [a] -> m [b]
+concatMapM f xs = liftM concat (mapM f xs)
 
 -- From wiki (http://www.haskell.org/haskellwiki/Generic_number_type)
 (^!) :: Num a => a -> Int -> a
-(^!) x n = x^n
+(^!) = (^)
 
 isqrt :: Int64 -> Int64
 isqrt 0 = 0
 isqrt 1 = 1
-isqrt n =
-  let twopows = iterate (^!2) 2
-      (lowerRoot, lowerN) =
-        last $ takeWhile ((n>=) . snd) $ zip (1:twopows) twopows
-      newtonStep x = div (x + div n x) 2
-      iters = iterate newtonStep (isqrt (div n lowerN) * lowerRoot)
-      isRoot r  =  r^!2 <= n && n < (r+1)^!2
-  in  head $ dropWhile (not . isRoot) iters
+isqrt n = head $ dropWhile (not . isRoot) iters where
+  twopows = iterate (^!2) 2
+  (lowerRoot, lowerN) = last $ takeWhile ((n>=) . snd) $ zip (1:twopows) twopows
+  newtonStep x = div (x + div n x) 2
+  iters = iterate newtonStep (isqrt (div n lowerN) * lowerRoot)
+  isRoot r = r^!2 <= n && n < (r+1)^!2
