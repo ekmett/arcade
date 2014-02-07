8c6c85f6f5659346349ff14e2a15ff83bcad1f34
Edward Kmett <ekmett@gmail.com>
2014-02-06 19:29:37 -0500

    more physics stuff

--- static/bsp.js
+++ static/bsp.js
@@ -2,17 +2,27 @@
 
 define([],function() {
 
+var Edge = bsp.Edge = function(front,back) {
+  this.front = front;
+  this.back = back;
+};
+
 var bsp = {
   epsilon : 0.01 // 1cm
 };
 
-var Node = bsp.Node = function(nx,ny,nz,d,f,b) {
+// we can always position the normal so that forward is toward the camera, since it is fixed axonometric
+// we can number the bsp cells front to back
+// then we can sort all bodies by bsp cell # and then internally by z to get a good ordering
+// on the plus side the sort by bsp can be a mutable mess.
+
+var Node = bsp.Node = function(nx,ny,d,f,b) {
   this.nx = nx; // normal
   this.ny = ny;
-  this.nz = nz;
   this.d  = d;  // distance from origin
   this.front = front;
   this.back  = back;
+  this.bodes = [];
 };
 
 Node.prototype = {
@@ -20,16 +30,23 @@ Node.prototype = {
     var d = x*this.nx+y*this.ny+z*this.nz+this.d;
     if (d > bsp.epsilon)  return this.front.classify(x,y);
     if (d < -bsp.epsilon) return this.back.classify(x,y);
-    return this.front.classify(x,y).concat(this.back.classify(x,y));
+    var f = this.front.classify(x,y);
+    var b = this.back.classify(x,y);
+    return edge(f,b);
+  },
+  draw: function(x,y,z) {
   }
 };
 
 var Leaf = bsp.Leaf = function(contents) {
-  this.contents = contents;
+  this.contents = {};
 };
 
 Leaf.prototype = {
   classify: function(x,y) { return this.contents; }
+};
+
+var Open = bsp.Open = function(floor, ceiling) {
 };
 
 return bsp;
--- static/clipping.js
+++ static/clipping.js
@@ -1,13 +1,14 @@
 define([], function() {
 
 // a swept bounding box.
-var Clipping = function(box,dx,dy,dz) {
+var Clipping = function(it,x1,y1,z1,x2,y2,z2,bounce) {
   this.box = box; // starting box
   this.dx = dx;
   this.dy = dy;
   this.dz = dz;
-  this.aabb = box.union(box.translate(dx,dy));
+  this.aabb = box.union(box.translate(dx,dy,dz));
 
+/*
   // 012
   // 345
   // 678
@@ -17,11 +18,14 @@ var Clipping = function(box,dx,dy,dz) {
   else if (dx === 0) direction += 1;
   if (dy > 0) direction += 6;
   else if (dy === 0) direction += 3;
+  if (dz > 0) direction += 18;
+  else if (dz === 0) direction += 9;
   this.direction = direction;
+*/
 };
 
-var clipping = function (box,dx,dy) {
-  return new Clipping(box,dx,dy);
+var clipping = function (box,dx,dy,dz) {
+  return new Clipping(box,dx,dy,dz);
 };
 
 clipping.Clipping = Clipping;
